0a082c1dd65e90f98066c843fdda1e0e46cf640d
mcumm64 <mcumm64@gmail.com>
2018-11-19 23:38:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,9 @@
-// keys = artist, song = value. Limitation = everything is a string
-var playlist = {['Amy Winehouse']: "Rehab", ['No Doubt']: }
+/* keys = artist, song = value. Limitation = everything is a string.
+* for non-simple strings use brackets!
+*/
+var playlist = {['Amy Winehouse']: "Rehab", 
+['No Doubt']: "Don't Speak"};
+
+function updatePlaylist (object, artist , song) {
+playlist[]
+}
